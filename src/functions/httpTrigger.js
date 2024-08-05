a7dd0dc99e46881d15d4249ebd955f6966decf80
Sam <sam@Annies-MacBook-Air.local>
2024-08-04 22:43:03 -0700

    commit 2

--- src/functions/httpTrigger.js
+++ src/functions/httpTrigger.js
@@ -6,7 +6,7 @@ app.http('httpTrigger', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+        const name = request.query.get('name') || await request.text() || 'world.';
 
         return { body: `Hello, ${name}!` };
     }
